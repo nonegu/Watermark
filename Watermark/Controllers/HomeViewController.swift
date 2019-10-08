db6b139e7509a7d09fd61941dacedbfe247c1206
Ender Güzel <enderguzel@gmail.com>
2019-10-08 14:28:31 +0300

    feat: Implement CollectionView for Categories

--- Watermark/Controllers/HomeViewController.swift
+++ Watermark/Controllers/HomeViewController.swift
@@ -9,13 +9,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
+    // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationController?.navigationBar.prefersLargeTitles = true
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
     }
 
 
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    
 }
 
