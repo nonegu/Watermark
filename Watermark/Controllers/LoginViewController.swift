29975d632e4eeb5fdae7ecf07ac24936d177f563
Ender Güzel <enderguzel@gmail.com>
2019-10-09 16:49:05 +0300

    feat: Create LoginViewController

---
+++ Watermark/Controllers/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Watermark
+//
+//  Created by Ender Güzel on 9.10.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginOrRegisterButton: UIButton!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSegmentedControlTextAttributes()
+        loginOrRegisterButton.layer.cornerRadius = 5
+    }
+    
+    @IBAction func loginOrRegisterPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "LoginSuccess", sender: self)
+    }
+    
+    func setupSegmentedControlTextAttributes() {
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        segmentedControl.setTitleTextAttributes(titleTextAttributes, for: .selected)
+    }
+    
+    
+}
