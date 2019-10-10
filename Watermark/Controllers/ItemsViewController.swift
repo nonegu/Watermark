95d032bdd8e786fe7450383f88aafe915b493a98
Ender Güzel <enderguzel@gmail.com>
2019-10-10 21:18:50 +0300

    feat: Create ItemsViewController

---
+++ Watermark/Controllers/ItemsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ItemsViewController.swift
+//  Watermark
+//
+//  Created by Ender Güzel on 10.10.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController {
+    
+    // MARK: Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
