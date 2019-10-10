e8a79ff004202bb251037d6e344fe60bd27fadf5
Ender Güzel <enderguzel@gmail.com>
2019-10-10 18:08:37 +0300

    feat: Setup AddItemViewController with PickerView

--- Watermark/Controllers/AddItemViewController.swift
+++ Watermark/Controllers/AddItemViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import RealmSwift
 
 class AddItemViewController: UIViewController {
     
+    // MARK: Outlets
+    @IBOutlet weak var itemTitle: UITextField!
+    @IBOutlet weak var itemCategory: UITextField!
+    @IBOutlet weak var dueDate: UIDatePicker!
     
+    // MARK: Properties
+    let realm = try! Realm()
+    var category: Category?
+    var allCategories: Results<Category>?
+    let pickerView = UIPickerView()
+    lazy var pickerViewToolbar: UIToolbar = {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: pickerView.frame.size.width, height: 25))
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(toolbarDoneButtonPressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(toolbarCancelButtonPressed))
+        toolbar.setItems([cancelButton, flexibleSpace, doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        toolbar.sizeToFit()
+        return toolbar
+    }()
+    
+    // MARK: Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCategoryTextField()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
         print("add item presented")
     }
     
@@ -20,6 +48,52 @@ class AddItemViewController: UIViewController {
         dismiss(animated: true) {
             print("handle save here")
         }
+    }
+    
+    func setupCategoryTextField() {
+        if category == nil {
+            allCategories = realm.objects(Category.self)
+            itemCategory.inputView = pickerView
+            itemCategory.inputAccessoryView = pickerViewToolbar
+        } else {
+            itemCategory.text = category?.name
+            itemCategory.isEnabled = false
+        }
+    }
+    
+    @objc func toolbarDoneButtonPressed() {
+        let row = pickerView.selectedRow(inComponent: 0)
+        itemCategory.text = allCategories![row].name
+        itemCategory.resignFirstResponder()
+    }
+    
+    @objc func toolbarCancelButtonPressed() {
+        itemCategory.resignFirstResponder()
+    }
+    
+    
+}
+
+extension AddItemViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return allCategories?.count ?? 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if allCategories?.count == 0 {
+            return "No categories added yet"
+        } else {
+            return allCategories?[row].name
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        itemCategory.text = allCategories![row].name
     }
     
     
