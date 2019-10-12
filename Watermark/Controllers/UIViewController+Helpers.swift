4dd30bcade421be43712725bec654c526292f5d5
Ender Güzel <enderguzel@gmail.com>
2019-10-12 12:13:39 +0300

    feat: Create notification methods for a new or updated item

--- Watermark/Controllers/UIViewController+Helpers.swift
+++ Watermark/Controllers/UIViewController+Helpers.swift
@@ -17,4 +17,16 @@ extension UIViewController {
         present(alertVC, animated: true, completion: nil)
     }
     
+    func subscribeToItemNotifications(selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: .didAddOrUpdateItem, object: nil)
+    }
+    
+    func unsubscribeToItemNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .didAddOrUpdateItem, object: nil)
+    }
+    
+}
+
+extension Notification.Name {
+    static let didAddOrUpdateItem = Notification.Name("didAddOrUpdateItem")
 }
