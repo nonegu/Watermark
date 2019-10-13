03da8eb81e1883b2795ca154d08cf32a7b67d1ba
Ender Güzel <enderguzel@gmail.com>
2019-10-13 18:09:14 +0300

    feat: Reload user items when HomeVC notified for an update or addition

--- Watermark/Controllers/HomeViewController.swift
+++ Watermark/Controllers/HomeViewController.swift
@@ -39,6 +39,7 @@ class HomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         categoryCollectionView.reloadData()
+        loadUserItems()
         itemTableView.reloadData()
         subscribeToItemNotifications(selector: #selector(itemAddedOrUpdated))
     }
